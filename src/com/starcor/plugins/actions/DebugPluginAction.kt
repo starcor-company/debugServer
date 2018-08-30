886bc67ad550839ddafde58958376ee308f2243f
mrlsm <1057702942@qq.com>
2018-08-30 11:11:00 +0800

    初始化提交

---
+++ src/com/starcor/plugins/actions/DebugPluginAction.kt
@@ -0,0 +1,16 @@
+package com.starcor.plugins.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+/**
+ * Copyright (c) 2018, 北京视达科科技有限责任公司 All rights reserved.
+ * author：MrLSM
+ * date：2018/8/13
+ * description：
+ */
+class DebugPluginAction : AnAction() {
+    override fun actionPerformed(anActionEvent: AnActionEvent) {
+
+    }
+}
